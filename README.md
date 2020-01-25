6f5749b66aea9cf942ed45159f40c0ef7e5c8f04
Nikolas Graham <Lichtschwert@live.com>
2020-01-24 16:07:02 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-This is a fan made project to develop useful tools for the amazing game of TemTem, by Crema.
-
-Site can be found here: https://nikolasgraham.github.io/TemTemTools/
-
-This is in no way affiliated with the company or game, just a fan's project.
---
+++ precache-manifest.4a3e770c5a1c7311bd97157ed8ad3c13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4034cbcb6ebcbc87a6ff53f6cb5e2f40",
+    "url": "/TemTemTools/index.html"
+  },
+  {
+    "revision": "0dc225d8a406e3c4c43d",
+    "url": "/TemTemTools/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f575aa43cef2c89d7ada",
+    "url": "/TemTemTools/static/css/main.b64d7a5c.chunk.css"
+  },
+  {
+    "revision": "0dc225d8a406e3c4c43d",
+    "url": "/TemTemTools/static/js/2.39af42fa.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/TemTemTools/static/js/2.39af42fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f575aa43cef2c89d7ada",
+    "url": "/TemTemTools/static/js/main.4dd11720.chunk.js"
+  },
+  {
+    "revision": "49470e90d1f3233da9f3",
+    "url": "/TemTemTools/static/js/runtime-main.c6587e3b.js"
+  }
+]);
